7f19f4013a413162dc549a71af638e370c8c676e
ChompyWarrior <hafizhizdiharm@gmail.com>
2023-04-15 17:15:45 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata Diri</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+    <div class="container border p-5 mt-5 rounded">
+        <div class="container">
+            <div class="fs-1">
+                <h1>Biodata Diri - FSWD 1</h1>
+            </div>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-5 mt-5">
+                        <img src="assets/img/foto.jpg" class="img-thumbnail rounded-circle" alt="test">
+                    </div>
+                    <div class="container col-5 text-start">
+                        <h1>Hafidz Izdihar Muslim</h1>
+                    </div>
+                </div>
+                <div class="fs-5 mt-5 border p-3 bg-body">
+                    <h1>Nama</h1>
+                    <p>Hafidz Izdihar Muslim</p>
+                    <h1>Tempat Tanggal Lahir</h1>
+                    <p>Tangerang, 21 April 2002</p>
+                    <h1>Alamat</h1>
+                    <p>Banten,Tangerang Selatan,Pondok Aren,Pd.Jati,Jl.Arrahmah RT001/RW013 NO.28</p>
+                <h1>Riwayat Pendidikan</h1>
+                <p>SMA : SMAN 5 Tangerang Selatan, Saat Ini : S1SI Sistem Informasi UPNVJ</p>
+                <h1>Hobby</h1>
+                <p>Membaca & Menulis</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
